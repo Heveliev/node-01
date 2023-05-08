e19c1e1667fa5eed6a6512b81b2a986d58a7eb98
Anatolii Heveliev <109628656+Heveliev@users.noreply.github.com>
2023-05-08 13:42:53 +0300

    add try catch

--- db/contacts.js
+++ db/contacts.js
@@ -7,17 +7,28 @@ const updateContatcs = async(contacts) => fs.writeFile(contactsPath, JSON.string
 
 
 async function listContacts () {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data)
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data)
+  } catch (error) {
+    throw new Error(error.message)
+  }
+ 
   }
   
   async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const result = contacts.find(item=> item.id === contactId)
-    return result || null
+    try {
+      const contacts = await listContacts();
+      const result = contacts.find(item=> item.id === contactId)
+      return result || null
+    } catch (error) {
+      throw new Error(error.message)
+    }
+    
   }
   
 async  function removeContact(contactId) {
+  try {
     const contacts = await listContacts();
     const idx = contacts.findIndex(item => item.id === contactId);
     if(idx === -1){
@@ -26,9 +37,14 @@ async  function removeContact(contactId) {
     const [result] = contacts.splice(idx, 1);
     await updateContatcs(contacts)
     return result;
+  } catch (error) {
+    throw new Error(error.message)
+  }
+   
   }
   
 async function addContact(name, email, phone) {
+  try { 
     const contacts = await listContacts(); 
     const newContact = {
       id: nanoid(),
@@ -39,6 +55,11 @@ async function addContact(name, email, phone) {
     contacts.push(newContact);
     await updateContatcs(contacts);
     return newContact
+    
+  } catch (error) {
+    throw new Error(error.message)
+  }
+   
   }
 module.exports = {
     listContacts,
